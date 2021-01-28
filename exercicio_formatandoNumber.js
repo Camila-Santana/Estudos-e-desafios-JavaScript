0fb51ae9120565268d8518e2316f3600afbcd113
Camila-Santana <tst.santana.camila@gmail.com>
2021-01-28 10:59:45 -0300

    exercícios
    comandos: document.write
    document.writeiL
    toUpperCase
    toLowerCase
    length
    toFixed
    replace

---
+++ exercicio_formatandoNumber.js
@@ -0,0 +1,15 @@
+Welcome to Node.js v14.15.1.
+Type ".help" for more information.
+> var n1 = 1545.5
+undefined
+> n1
+1545.5
+> n1.toFixed(2)
+'1545.50'
+> n1.toFixed(2).replace('.',',') 
+'1545,50'
+
+//exercício desenvolvido no Node.js//
+//declarei uma variável NUMBER com apenas UMA casa decimal e UM PONTO//
+//transformei essa casa decimal em DUAS CASAS DECIMAIS//
+//transformei UM PONTO em VÍRGULA//
