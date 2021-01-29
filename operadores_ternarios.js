c4a9864bfd5c1313454e102b33f10fbfb0798c68
Camila-Santana <tst.santana.camila@gmail.com>
2021-01-29 10:17:13 -0300

    Create operadores_ternarios.js
    atualização nos estudos

---
+++ operadores_ternarios.js
@@ -0,0 +1,20 @@
+// OPERADORES TERNÁRIOS //
+
+// EXERCÍCIO DESENVOLVIDO NO NODE //
+
+Welcome to Node.js v14 .15 .1.
+Type ".help"
+for more information. >
+    var media = 5.5
+undefined
+    >
+    media
+5.5
+    >
+    media >= 7 ? 'APROVADO' : 'REPROVADO'
+'REPROVADO'
+
+// ? faço uma pergunta para minha condição
+// : dou duas condições para que seja executada
+// dependendo da minha pergunta ? 
+// será executada através do : aquela situação que for aceitável/condizente com a pergunta
